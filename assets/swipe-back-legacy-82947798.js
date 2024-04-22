c30fe4e4f07782f9a1754737282cba73758ba0f1
Veronika0509 <veronikachika0509@gmail.com>
2024-04-22 14:08:29 +0200

    Updates

--- assets/swipe-back-legacy-82947798.js
+++ assets/swipe-back-legacy-82947798.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-1354bbdd.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-ed98bd09.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
