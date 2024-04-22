8520b22afd7f8e50d753dd9dd16f645cf007035d
Veronika0509 <veronikachika0509@gmail.com>
2024-04-22 12:20:27 +0200

    Updates

--- assets/ios.transition-legacy-b583767d.js
+++ assets/ios.transition-legacy-b583767d.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-86c0499e.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-712dd400.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
