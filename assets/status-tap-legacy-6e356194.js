ddae3efdbc74defb2084760b04c7fa5c229b0901
Veronika0509 <veronikachika0509@gmail.com>
2024-04-22 14:02:00 +0200

    Updates

--- assets/status-tap-legacy-6e356194.js
+++ assets/status-tap-legacy-6e356194.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-f4fbfbad.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-44bcf320.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
