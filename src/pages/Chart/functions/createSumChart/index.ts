30ab21f98b833d0d3cc0f17a55cff9bdc5849888
Veronika0509 <veronikachika0509@gmail.com>
2024-05-10 21:48:20 +0200

    Sum Chart

--- src/pages/Chart/functions/createSumChart/index.ts
+++ src/pages/Chart/functions/createSumChart/index.ts
@@ -94,6 +94,183 @@ export const createSumChart = (chartData: any, budgetLines: any, root: any) => {
       series.appear();
     }
 
+// Inside
+    // Lines
+    console.log(budgetLines)
+    if (budgetLines[2].value) {
+      let seriesRangeDataItem = yAxis.makeDataItem({
+        value: budgetLines[2].value
+      });
+      series.createAxisRange(seriesRangeDataItem);
+      seriesRangeDataItem.get("grid").setAll({
+        strokeOpacity: 1,
+        visible: true,
+        stroke: am5.color(0x000000),
+        strokeDasharray: [15, 15],
+      });
+      seriesRangeDataItem.get("label").setAll({
+        text: budgetLines[2].label,
+        fill: am5.color(0x000000),
+        background: am5.RoundedRectangle.new(root.current, {
+          fill: am5.color(0xffffff)
+        }),
+        inside: true,
+        fontSize: 12,
+        centerX: 0,
+        centerY: "0%",
+        y: -10,
+        visible: true
+      });
+    }
+    if (budgetLines[3].value) {
+      let seriesRangeDataItem = yAxis.makeDataItem({
+        value: budgetLines[3].value
+      });
+      series.createAxisRange(seriesRangeDataItem);
+      seriesRangeDataItem.get("grid").setAll({
+        strokeOpacity: 1,
+        visible: true,
+        stroke: am5.color(0x000000),
+        strokeDasharray: [15, 15],
+      });
+      seriesRangeDataItem.get("label").setAll({
+        text: budgetLines[3].label,
+        fill: am5.color(0x000000),
+        background: am5.RoundedRectangle.new(root.current, {
+          fill: am5.color(0xffffff)
+        }),
+        inside: true,
+        fontSize: 12,
+        centerX: 0,
+        centerY: 0,
+        visible: true
+      });
+    }
+
+    let middleBottomLine = yAxis.makeDataItem({
+      value: budgetLines[4].value
+    });
+    series.createAxisRange(middleBottomLine);
+    middleBottomLine.get("grid").setAll({
+      strokeOpacity: 1,
+      visible: true,
+      stroke: am5.color(0xCCCC00),
+      strokeDasharray: [4, 4],
+    });
+    middleBottomLine.get("label").setAll({
+      text: budgetLines[4].label,
+      fill: am5.color(0x000000),
+      background: am5.RoundedRectangle.new(root.current, {
+        fill: am5.color(0xffffff)
+      }),
+      inside: true,
+      fontSize: 12,
+      centerX: 0,
+      centerY: 0,
+      visible: true
+    });
+
+    let topMiddleLine = yAxis.makeDataItem({
+      value: budgetLines[1].value
+    });
+    series.createAxisRange(topMiddleLine);
+    topMiddleLine.get("grid").setAll({
+      strokeOpacity: 1,
+      visible: true,
+      stroke: am5.color(0xCCCC00),
+      strokeDasharray: [4, 4],
+    });
+    topMiddleLine.get("label").setAll({
+      text: budgetLines[1].label,
+      fill: am5.color(0x000000),
+      background: am5.RoundedRectangle.new(root.current, {
+        fill: am5.color(0xffffff)
+      }),
+      inside: true,
+      fontSize: 12,
+      centerX: 0,
+      centerY: 0,
+      visible: true
+    });
+
+    // Regions
+    let topBudgetRegion = yAxis.makeDataItem({
+      value: budgetLines[1].value,
+      endValue: 100
+    });
+    series.createAxisRange(topBudgetRegion);
+    topBudgetRegion.get("grid").setAll({
+      strokeOpacity: 1,
+      visible: true,
+      stroke: am5.color(0xCCCC00),
+      strokeDasharray: [4, 4]
+    });
+    topBudgetRegion.get("axisFill").setAll({
+      fill: am5.color(0x02c5fd),
+      fillOpacity: 0.2,
+      visible: true
+    });
+    topBudgetRegion.get("label").setAll({
+      text: budgetLines[1].label,
+      fill: am5.color(0x000000),
+      background: am5.RoundedRectangle.new(root.current, {
+        fill: am5.color(0xffffff)
+      }),
+      location: 0,
+      inside: true,
+      fontSize: 12,
+      centerX: 0,
+      centerY: 0,
+      visible: true
+    });
+
+    let middleBudgetRegion = yAxis.makeDataItem({
+      value: budgetLines[1].value,
+      endValue: budgetLines[4].value
+    });
+    series.createAxisRange(middleBudgetRegion);
+    middleBudgetRegion.get("grid").setAll({
+      strokeOpacity: 1,
+      visible: true,
+      stroke: am5.color(0xCC0000),
+      strokeDasharray: [4, 4]
+    });
+    middleBudgetRegion.get("axisFill").setAll({
+      fill: am5.color(0x08f908),
+      fillOpacity: 0.2,
+      visible: true
+    });
+
+    let bottomBudgetRegion = yAxis.makeDataItem({
+      value: budgetLines[4].value,
+      endValue: 0
+    });
+    series.createAxisRange(bottomBudgetRegion);
+    bottomBudgetRegion.get("grid").setAll({
+      strokeOpacity: 1,
+      visible: true,
+      stroke: am5.color(0xCCCC00),
+      strokeDasharray: [4, 4]
+    });
+    bottomBudgetRegion.get("axisFill").setAll({
+      fill: am5.color(0xf6363b),
+      fillOpacity: 0.2,
+      visible: true
+    });
+    bottomBudgetRegion.get("label").setAll({
+      text: budgetLines[4].label,
+      fill: am5.color(0x000000),
+      background: am5.RoundedRectangle.new(root.current, {
+        fill: am5.color(0xffffff)
+      }),
+      location: 0,
+      inside: true,
+      fontSize: 12,
+      centerX: 0,
+      centerY: 0,
+      visible: true
+    });
+
 // Add cursor
     let cursor = chart.set("cursor", am5xy.XYCursor.new(root.current, {
       behavior: "zoomX",
