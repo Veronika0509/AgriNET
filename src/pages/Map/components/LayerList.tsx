bf91b55f28e9b62cb587cab8739f7310ab1710c7
Veronika <100239671+Veronika0509@users.noreply.github.com>
2024-12-03 12:46:14 +0100

    refactor: Simplify LayerList component and remove unused imports

--- src/pages/Map/components/LayerList.tsx
+++ src/pages/Map/components/LayerList.tsx
@@ -1,45 +1,1 @@
-import s from '../style.module.css'
-import {IonCheckbox, IonItem} from "@ionic/react";
-
-const LayerList = (props: any) => {
-  let layers: string[] = []
-
-  props.siteList.map((site: any) => {
-    if (site.name === props.secondMap) {
-      site.layers.map((layer: any) => {
-        if (!layers.includes(layer.name)) {
-          layers.push(layer.name)
-        }
-      })
-    }
-  })
-
-
-  const toggleLayer = (checkbox: any) => {
-    props.overlays.forEach((overlay: any) => {
-      if (checkbox.target.innerText === overlay.layerName) {
-        if (checkbox.detail.checked) {
-          overlay.show()
-        } else {
-          overlay.hide()
-        }
-      }
-    })
-  }
-
-  return (
-    <div className={s.layersListWrapper}>
-      <div className={s.layers_checkbox}>
-        {
-          layers.map((layer: string) => (
-            <IonItem key={layer}>
-              <IonCheckbox checked justify="space-between" onIonChange={(checkbox) => toggleLayer(checkbox)}>{layer}</IonCheckbox>
-            </IonItem>
-          ))
-        }
-      </div>
-    </div>
-  )
-}
-
-export default LayerList
+import s from '../style.module.css'import {IonCheckbox, IonItem} from "@ionic/react";import {useEffect, useState} from "react";import login from "../../Login";import {CollisionResolver} from "./CollisionResolver";const LayerList = (props: any) => {  let layers: string[] = []  props.siteList.map((site: any) => {    if (site.name === props.secondMap) {      site.layers.map((layer: any) => {        if (!layers.includes(layer.name)) {          layers.push(layer.name)        }      })    }  })  const toggleLayer = (checkbox: any) => {    props.overlays.forEach((overlay: any) => {      if (checkbox.target.innerText === overlay.layerName) {        if (checkbox.detail.checked) {          overlay.show()        } else {          overlay.hide()        }      }    })  }  return (    <div className={s.layersListWrapper}>      <div className={s.layers_checkbox}>        {          layers.map((layer: string) => (            <IonItem key={layer}>              <IonCheckbox justify="space-between" onIonChange={(checkbox) => toggleLayer(checkbox)}>{layer}</IonCheckbox>            </IonItem>          ))        }      </div>    </div>  )}export default LayerList
