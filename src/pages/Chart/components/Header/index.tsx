fa64f34a8df5f6678ee2f8a88fa28a6732cee996
Veronika <100239671+Veronika0509@users.noreply.github.com>
2025-01-10 13:26:51 +0100

    refactor: Simplify Header component by removing mobile-specific popover logic

--- src/pages/Chart/components/Header/index.tsx
+++ src/pages/Chart/components/Header/index.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
-import {IonButton, IonButtons, IonHeader, IonIcon, IonTitle, IonToolbar, IonPopover} from "@ionic/react";
-import { useMediaQuery } from '@react-hook/media-query'
+import React from 'react'
+import {IonButton, IonButtons, IonHeader, IonIcon, IonTitle, IonToolbar} from "@ionic/react";
 import {back} from "../../functions/back";
 import s from "../../style.module.css";
 import {arrowBackOutline} from "ionicons/icons";
@@ -12,9 +11,6 @@ import {settings} from "ionicons/icons";
 
 const Header = (props: any) => {
   const history = useHistory();
-  const [showPopover, setShowPopover] = useState(false);
-  const [popoverEvent, setPopoverEvent] = useState<any>(null);
-  const isMobile = useMediaQuery('(max-width: 768px)');
 
   const onBackClick = () => {
     if (props.type === 'chartPage') {
@@ -49,65 +45,24 @@ const Header = (props: any) => {
           )}
         </IonTitle>
         {props.chartType === 'valve' && (
-          <>
-            {isMobile ? (
-              <IonButtons slot='end'>
-                <IonButton
-                  onClick={(e) => {
-                    e.persist();
-                    setPopoverEvent(e);
-                    setShowPopover(true);
-                  }}
-                >
-                  <IonIcon icon={settings} className={s.header_valveButtonIcon}></IonIcon>
-                </IonButton>
-                <IonPopover
-                  isOpen={showPopover}
-                  event={popoverEvent}
-                  onDidDismiss={() => setShowPopover(false)}
-                >
-                  <IonButton expand="block" fill="clear">
-                    <IonIcon icon={addOutline} slot="start"></IonIcon>
-                    Create
-                  </IonButton>
-                  <IonButton expand="block" fill="clear">
-                    <IonIcon icon={trash} slot="start"></IonIcon>
-                    Clear
-                  </IonButton>
-                  <IonButton expand="block" fill="clear" onClick={() => {
-                    props.setValveArchive(true);
-                    setShowPopover(false);
-                  }}>
-                    <IonIcon icon={timeOutline} slot="start"></IonIcon>
-                    Open archive
-                  </IonButton>
-                  <IonButton expand="block" fill="clear">
-                    <IonIcon icon={settings} slot="start"></IonIcon>
-                    Settings
-                  </IonButton>
-                </IonPopover>
-              </IonButtons>
-            ) : (
-              <IonButtons slot='end' className={s.header_valveButtons}>
-                <IonButton>
-                  <IonIcon icon={addOutline} className={s.header_valveButtonIcon}></IonIcon>
-                  Create
-                </IonButton>
-                <IonButton>
-                  <IonIcon icon={trash} className={s.header_valveButtonIcon}></IonIcon>
-                  Clear
-                </IonButton>
-                <IonButton onClick={() => props.setValveArchive(true)}>
-                  <IonIcon icon={timeOutline} className={s.header_valveButtonIcon}></IonIcon>
-                  Open archive
-                </IonButton>
-                <IonButton>
-                  <IonIcon icon={settings} className={s.header_valveButtonIcon}></IonIcon>
-                  Settings
-                </IonButton>
-              </IonButtons>
-            )}
-          </>
+          <IonButtons slot='end' className={s.header_valveButtons}>
+            <IonButton>
+              <IonIcon icon={addOutline} className={s.header_valveButtonIcon}></IonIcon>
+              Create
+            </IonButton>
+            <IonButton>
+              <IonIcon icon={trash} className={s.header_valveButtonIcon}></IonIcon>
+              Clear
+            </IonButton>
+            <IonButton onClick={() => props.setValveArchive(true)}>
+              <IonIcon icon={timeOutline} className={s.header_valveButtonIcon}></IonIcon>
+              Open archive
+            </IonButton>
+            <IonButton>
+              <IonIcon icon={settings} className={s.header_valveButtonIcon}></IonIcon>
+              Settings
+            </IonButton>
+          </IonButtons>
         )}
       </IonToolbar>
     </IonHeader>
