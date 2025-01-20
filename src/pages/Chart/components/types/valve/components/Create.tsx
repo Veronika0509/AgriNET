6a17b72c0037b78ec693c22d0d6a06a8959e5c2c
Veronika <100239671+Veronika0509@users.noreply.github.com>
2025-01-20 15:39:16 +0100

    feat: Add valve creation form with duration, start/stop times, timezone, and pulse irrigation options

--- src/pages/Chart/components/types/valve/components/Create.tsx
+++ src/pages/Chart/components/types/valve/components/Create.tsx
@@ -1,9 +1,23 @@
 import s from "../style.module.css";
 import Header from "../../../Header";
-import {IonContent, IonDatetime, IonDatetimeButton, IonItem, IonLabel, IonModal, IonSpinner} from "@ionic/react";
+import {
+  IonCheckbox,
+  IonContent,
+  IonDatetime,
+  IonDatetimeButton, IonInput,
+  IonItem,
+  IonLabel,
+  IonModal,
+  IonSelect, IonSelectOption,
+  IonSpinner
+} from "@ionic/react";
 import React, {useCallback, useEffect, useRef, useState} from "react";
 
 export const Create = (props: any) => {
+  const [isPulseIrrigation, setIsPulseIrrigation] = useState(false)
+  const startTime = new Date(new Date().getTime() + 2 * 60 * 1000)
+  const stopTime = new Date(startTime.getTime() + 6 * 60 * 60 * 1000)
+
   return (
     <IonModal isOpen={props.valveCreate} className={s.createModal}>
       <Header type='valveCreateModal' sensorId={props.sensorId} setValveCreate={props.setValveCreate}/>
@@ -12,15 +26,43 @@ export const Create = (props: any) => {
           <IonItem className={s.createModalItem}>{props.config?.names[0] ? props.config?.names[0] : 'Valve'}</IonItem>
           <IonItem className={s.createModalItem}>
             <IonLabel className={s.createModalItemLabel}>Duration</IonLabel>
-            <IonDatetimeButton datetime="datetime"></IonDatetimeButton>
-            <IonModal keepContentsMounted={true} className={s.timePickerModal}>
-              <IonDatetime id="datetime" className={s.createModalItemDatetime} presentation='time'></IonDatetime>
+            <IonDatetimeButton datetime="durationDatetime"></IonDatetimeButton>
+            <IonModal keepContentsMounted={true} className={s.createTimePickerModal}>
+              <IonDatetime id="durationDatetime" presentation='time' show-default-buttons="true" value={'06:00'}></IonDatetime>
             </IonModal>
           </IonItem>
-          <IonItem className={s.createModalItem}></IonItem>
-          <IonItem className={s.createModalItem}></IonItem>
-          <IonItem className={s.createModalItem}></IonItem>
-          <IonItem className={s.createModalItem}></IonItem>
+          <IonItem className={s.createModalItem}>
+            <IonLabel className={s.createModalItemLabel}>Start Time</IonLabel>
+            <IonDatetimeButton datetime="startDatetime"></IonDatetimeButton>
+            <IonModal keepContentsMounted={true} className={s.createTimePickerModal}>
+              <IonDatetime id="startDatetime" presentation='date-time' show-default-buttons="true" value={startTime.toISOString()}></IonDatetime>
+            </IonModal>
+          </IonItem>
+          <IonItem className={s.createModalItem}>
+            <IonLabel className={s.createModalItemLabel}>Stop Time</IonLabel>
+            <IonDatetimeButton datetime="stopDatetime"></IonDatetimeButton>
+            <IonModal keepContentsMounted={true} className={s.createTimePickerModal}>
+              <IonDatetime id="stopDatetime" presentation='date-time' show-default-buttons="true" value={stopTime.toISOString()}></IonDatetime>
+            </IonModal>
+          </IonItem>
+          <IonItem className={`${s.createModalItem} ${s.createTimezoneItem}`}>
+            <IonLabel className={s.createModalItemLabel}>Current timezone</IonLabel>
+            <IonSelect placeholder="Timezone">
+              {Intl.supportedValuesOf('timeZone').map((timezone: any) => (
+                <IonSelectOption value={timezone}>{timezone}</IonSelectOption>
+              ))}
+            </IonSelect>
+          </IonItem>
+          <IonItem className={s.createModalItem}>
+            <IonCheckbox value={isPulseIrrigation} onIonChange={() => setIsPulseIrrigation(!isPulseIrrigation)} className={s.createModalItemCheckbox}>Pulse Irrigation</IonCheckbox>
+          </IonItem>
+          {isPulseIrrigation && (
+            <>
+            <IonItem className={`${s.createModalItem} ${s.createPulseCountItem}`}>
+              <IonInput label="Pulse Count" type='number'></IonInput>
+            </IonItem>
+            </>
+          )}
         </div>
       </IonContent>
     </IonModal>
