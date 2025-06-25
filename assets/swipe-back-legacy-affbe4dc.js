501d1bbc443f5083a28d3727f18c1cc3c3a56b63
Veronika <100239671+Veronika0509@users.noreply.github.com>
2025-06-25 12:01:21 +0200

    Updates

--- assets/swipe-back-legacy-affbe4dc.js
+++ assets/swipe-back-legacy-affbe4dc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-2176cb6d.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-a306b5c8.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
