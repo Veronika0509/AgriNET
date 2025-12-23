620251caf40eb09b3692796556ef279aef32ed0e
Veronika <100239671+Veronika0509@users.noreply.github.com>
2025-12-23 19:31:32 +0100

    Updates

--- assets/swipe-back-legacy-75d3b645.js
+++ assets/swipe-back-legacy-75d3b645.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-4aa17e9e.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-46527492.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
