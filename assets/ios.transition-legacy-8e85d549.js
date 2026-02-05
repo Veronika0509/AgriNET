be2ea720eb81ab3414166f3578a62ccff7d18ddb
Veronika <100239671+Veronika0509@users.noreply.github.com>
2026-02-05 20:44:30 +0100

    Updates

--- assets/ios.transition-legacy-8e85d549.js
+++ assets/ios.transition-legacy-8e85d549.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-2b19fcf2.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-6579bd04.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-ddba1307.js
+++ assets/swipe-back-legacy-ddba1307.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-2b19fcf2.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-6579bd04.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
