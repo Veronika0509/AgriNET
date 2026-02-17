eba54eb7f97d01905783991616dc0ae69fbd3fa7
Veronika <100239671+Veronika0509@users.noreply.github.com>
2026-02-17 18:52:47 +0100

    Updates

--- assets/md.transition-legacy-808c0fbc.js
+++ assets/md.transition-legacy-808c0fbc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-6579bd04.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-b61d2ca1.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
