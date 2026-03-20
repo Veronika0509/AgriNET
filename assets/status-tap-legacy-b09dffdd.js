c66b5a19dfef9cdf1193ae2e20f21a129ef1c8f9
Veronika <100239671+Veronika0509@users.noreply.github.com>
2026-03-20 16:37:03 +0100

    Updates

--- assets/status-tap-legacy-b09dffdd.js
+++ assets/status-tap-legacy-b09dffdd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-699e3500.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-a651ed1e.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-421c72ec.js
+++ assets/swipe-back-legacy-421c72ec.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-699e3500.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-a651ed1e.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
