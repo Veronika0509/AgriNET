82239deca2733ad680a1213940fd0d53884751e9
yurii <sllouyssgort@gmail.com>
2026-04-27 17:59:55 +0200

    Updates

--- assets/ios.transition-legacy-bc0a3e19.js
+++ assets/ios.transition-legacy-bc0a3e19.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-093216da.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-e4e49274.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
