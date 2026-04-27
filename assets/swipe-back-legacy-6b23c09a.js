b18f643fe0a014d6e1db7293ffb96b4e9e0d2bb2
yurii <sllouyssgort@gmail.com>
2026-04-27 17:40:34 +0200

    Updates

--- assets/swipe-back-legacy-6b23c09a.js
+++ assets/swipe-back-legacy-6b23c09a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-a651ed1e.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-093216da.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
