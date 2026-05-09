d6e6869e18730627f39545e7ea1b59188a066257
Veronika <100239671+Veronika0509@users.noreply.github.com>
2026-05-09 22:08:23 +0200

    Updates

--- assets/status-tap-legacy-44718ceb.js
+++ assets/status-tap-legacy-44718ceb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5943ee4e.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-aa1d2418.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
