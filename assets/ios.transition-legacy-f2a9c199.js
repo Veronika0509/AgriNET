b9a812bf029b452abeb57b55e260d56036149345
Veronika <100239671+Veronika0509@users.noreply.github.com>
2026-05-17 16:44:46 +0200

    Updates

--- assets/ios.transition-legacy-f2a9c199.js
+++ assets/ios.transition-legacy-f2a9c199.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-aa1d2418.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-61287602.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
