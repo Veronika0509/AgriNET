32084a6cc08d1f7c0975ffe8609d7f1334e8987f
Veronika <100239671+Veronika0509@users.noreply.github.com>
2026-08-01 22:28:20 +0200

    Updates

--- assets/ios.transition-legacy-f90f816d.js
+++ assets/ios.transition-legacy-f90f816d.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-1dace015.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-a4db12bb.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
